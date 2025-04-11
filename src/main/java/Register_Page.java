0692ecd89be2e297758f7889b579b34d16b50b58
Phanuwat <115919092+shibatj@users.noreply.github.com>
2025-04-12 02:03:26 +0700

    .

--- src/main/java/Register_Page.java
+++ src/main/java/Register_Page.java
@@ -37,7 +37,7 @@ public class Register_Page extends javax.swing.JFrame {
         jLabel1.setFont(new java.awt.Font("Segoe UI", 3, 36)); // NOI18N
         jLabel1.setForeground(new java.awt.Color(255, 255, 255));
         jLabel1.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
-        jLabel1.setText("Addmin Register");
+        jLabel1.setText("Admin Register");
 
         usr_reg.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
